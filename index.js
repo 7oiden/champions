088e4c89b505a0ddbdc441ea888a39e1ada77c98
Tommy Johnsen <7oiden@gmail.com>
2023-12-14 15:24:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://champions-36eff-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const messagesInDB = ref(database, "messages");
+
+const msgInputField = document.getElementById("input-field");
+const fromInputField = document.getElementById("from-field");
+const toInputField = document.getElementById("to-field");
+const publishBtn = document.getElementById("publish-btn");
+const msgList = document.getElementById("msg-list");
+
+publishBtn.addEventListener("click", addMsg);
+
+function addMsg() {
+
+  let inputValues = {
+    to: toInputField.value,
+    from: fromInputField.value,
+    msg: msgInputField.value,
+  };
+
+  if (inputValues) {
+    push(messagesInDB, inputValues);
+    clearInputFields();
+  }
+}
+
+onValue(messagesInDB, (snapshot) => {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    let reversedItemsArray = itemsArray.reverse();
+
+    clearMsgList();
+    // console.log(itemsArray);
+
+    reversedItemsArray.forEach((msg) => {
+      let currentMsg = msg;
+
+      addMgsToList(currentMsg);
+    });
+  }
+});
+
+function clearInputFields() {
+  msgInputField.value = "";
+  fromInputField.value = "";
+  toInputField.value = "";
+}
+
+function clearMsgList() {
+  msgList.innerHTML = "";
+}
+
+function addMgsToList(msg) {
+  console.log(msg);
+  let msgId = msg[0];
+  let msgValue = msg[1];
+
+  let numOfLikes = 0;
+
+  let newEl = document.createElement("li");
+
+  newEl.innerHTML += `
+    <div class="card">
+    <p class="name">From ${msgValue.from}</p>
+    <p>Message: ${msgValue.msg}</p>
+    <p class="name">To ${msgValue.to}</p>
+    <span>${numOfLikes}</span>
+    </div>`;
+
+  msgList.appendChild(newEl);
+}
